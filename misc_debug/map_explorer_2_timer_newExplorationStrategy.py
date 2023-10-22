ea95d1df0f95440bb3706327faa314f0b82c2a07
RR45890921 <143500683+RR45890921@users.noreply.github.com>
2023-10-22 23:50:35 +1000

    Update map_explorer_2_timer_newExplorationStrategy.py

--- misc_debug/map_explorer_2_timer_newExplorationStrategy.py
+++ misc_debug/map_explorer_2_timer_newExplorationStrategy.py
@@ -1,8 +1,10 @@
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import OccupancyGrid, Odometry
-from geometry_msgs.msg import PoseStamped
+from nav2_msgs.msg import BehaviorTreeLog
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 import time
 
@@ -10,12 +12,31 @@ class MappingNode(Node):
     def __init__(self):
         super().__init__('map_explorer_2')
 
-        # Subscribe to topics and create publishers
+        # Initialize variables and data structures
+        self.map_data = None
+        self.map_width = 0
+        self.map_height = 0
+        self.map_resolution = 0
+        self.map_origin_x = 0
+        self.map_origin_y = 0
+        self.map_2d_occupancy_array = None
+        self.robot_x = 0
+        self.robot_y = 0
+        self.robot_z = 0
+
+        # Initialize subscribers
         self.map_subscription = self.create_subscription(
             OccupancyGrid,
             '/map',
             self.map_callback,
             20)
+
+        self.bt_subscription = self.create_subscription(
+            BehaviorTreeLog,
+            'behavior_tree_log',
+            self.bt_callback,
+            20
+        )
 
         self.pose_subscription = self.create_subscription(
             Odometry,
@@ -24,117 +45,157 @@ class MappingNode(Node):
             10
         )
 
+        # Initialize publishers
         self.goal_publisher_ = self.create_publisher(
             PoseStamped,
             'goal_pose',
             10
         )
 
-        self.map_data = None
-        self.map_width = 0
-        self.map_height = 0
-        self.map_resolution = 0
-        self.map_origin_x = 0
-        self.map_origin_y = 0
-        self.map_2d_occupancy_array = None
-
-        self.robot_x = 0
-        self.robot_y = 0
-
+        # Initialize flags
+        self.bt_callback_check = False
+        self.pose_callback_check = False
         self.map_callback_check = False
-
-        # Initialize waypoint strategy
-        self.initial_exploration = True
-        self.waypoint_strategy = "furthest"
+        self.patience = False
 
         # Create a timer to run the robot_brain at a fixed rate (e.g., every 1 second)
-        self.timer_period = 1
+        self.timer_period = 1  # in seconds
         self.timer = self.create_timer(self.timer_period, self.robot_brain)
         self.last_robot_brain_time = time.time()
 
+    # Map callback to process map data
     def map_callback(self, msg):
         self.map_callback_check = True
+        self.patience = False
+
         self.map_data = msg
         self.map_width = self.map_data.info.width
         self.map_height = self.map_data.info.height
         self.map_resolution = self.map_data.info.resolution
         self.map_origin_x = self.map_data.info.origin.position.x
         self.map_origin_y = self.map_data.info.origin.position.y
+
         self.map_2d_occupancy_array = np.array(self.map_data.data).reshape((self.map_height, self.map_width))
 
+    # Odometry callback to update robot's pose
     def pose_callback(self, msg):
+        self.pose_callback_check = True
+
         self.robot_x = msg.pose.pose.position.x
         self.robot_y = msg.pose.pose.position.y
+        self.robot_z = msg.pose.pose.position.z
 
-    def score_waypoints_initial(self):
-        # Calculate the distance to the farthest frontier
-        farthest_frontier = max(self.map_unoc_unex_obst, key=lambda f: self.euclidean_distance(self.robot_x, self.robot_y, *f))
-        distance_to_farthest = self.euclidean_distance(self.robot_x, self.robot_y, *farthest_frontier)
+    # Behavior Tree callback to update robot's behavior
+    def bt_callback(self, msg):
+        self.bt_callback_check = True
+        pass
 
-        self.wall_frontier_scores = {}
-        for frontier in self.map_unoc_unex_obst:
-            x, y = frontier
-            score = 0
+    # Find unoccupied-unexplored frontiers
+    def find_waypoints(self):
+        # Your code for finding unoccupied-unexplored frontiers
+        pass
 
-            # Adjust score higher for distance to farthest frontier
-            distance_weight = 1.0  # Modify this weight to balance density and distance
-            distance_score = 1 / self.euclidean_distance(self.robot_x, self.robot_y, x, y)  # Adjust the scoring formula
+    # Score frontiers based on distance to robot
+    def score_waypoints(self):
+        # Your code for scoring frontiers based on distance to the robot
+        pass
 
-            score = distance_weight * distance_score
+    # Helper function to transform grid coordinates to map coordinates
+    def grid_to_map_coordinates(self, grid_coordinates_set):
+        map_coordinates_set = set()
+        for x, y in grid_coordinates_set:
+            map_x = x * self.map_resolution + self.map_origin_x
+            map_y = y * self.map_resolution + self.map_origin_y
+            map_coordinates_set.add((map_x, map_y))
+        return map_coordinates_set
+    
+    # Helper function to transform map coordinates to grid coordinates
+    def map_to_grid_coordinates(self, map_coordinates):
+        x, y = map_coordinates
+        grid_x = int((x - self.map_origin_x) / self.map_resolution)
+        grid_y = int((y - self.map_origin_y) / self.map_resolution)
+        return grid_x, grid_y
+    
+    # Send the highest-scoring waypoint as a goal
+    def send_waypoint(self):
+        # Your code to send the highest-scoring waypoint as a goal
+        pass
 
-            self.wall_frontier_scores[frontier] = score
-
-    def score_waypoints_subsequent(self):
-        self.dense_frontier_scores = {}
-        for frontier in self.map_unoc_unex:
-            x, y = frontier
-            score = 0   
-
-            # Adjust score higher for density of 
-            distance_weight = 1.0  # Modify this weight to control how much it prioritizes proximity
-            distance_score = 1 / self.euclidean_distance(self.robot_x, self.robot_y, x, y)  # Adjust the scoring formula
-
-            score = distance_weight * distance_score
-
-            self.dense_frontier_scores[frontier] = score
-
-    def euclidean_distance(self, x1, y1, x2, y2):
-        return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
-
+    # Robot brain to control the exploration strategy
     def robot_brain(self):
         current_time = time.time()
+        time_diff = current_time - self.last_robot_brain_time
 
-        if self.initial_exploration:
-            self.score_waypoints_initial()
-            self.waypoint_strategy = "furthest"
-            self.initial_exploration = False
-        else:
-            self.score_waypoints_subsequent()
-            self.waypoint_strategy = "closest"
-
-        if self.waypoint_strategy == "furthest":
-            frontier_to_visit = max(self.wall_frontier_scores, key=self.wall_frontier_scores.get)
-        else:
-            frontier_to_visit = min(self.dense_frontier_scores, key=self.dense_frontier_scores.get)
-
-        x, y = frontier_to_visit
-
-        # Create the waypoint message
-        waypoint_msg = PoseStamped()
-        waypoint_msg.header.frame_id = "map"
-
-        # If no waypoints, go to center of map
+        # If no map callback has happened, wait and send waypoint to origin in the meantime
         if not self.map_callback_check:
-            waypoint_msg.pose.position.x = 0.0
-            waypoint_msg.pose.position.y = 0.0
-        else:
-            waypoint_msg.pose.position.x = x
-            waypoint_msg.pose.position.y = y
+            return
+        
+        # Find waypoints, score them, and send the highest-scoring waypoint as a goal
+        self.find_waypoints()
+        self.score_waypoints()
+        self.send_waypoint()
+        self.plot_map()
 
-        if self.patience == False:
-            self.goal_publisher_.publish(waypoint_msg)
-
+        # Update the last robot_brain time
         self.last_robot_brain_time = current_time
+
+    # Function to plot the occupancy grid and frontier points
+    def plot_map(self):
+    # Create a colormap for unexplored (-1, red), unoccupied (0, green), and obstacle (100, blue)
+    cmap = plt.cm.colors.ListedColormap(['red', 'green', 'blue'])
+    bounds = [-1, 0, 100, 101]  # Define the color boundaries
+    norm = plt.cm.colors.BoundaryNorm(bounds, cmap.N)
+
+    # Create figure with two sublots: one for occupancy grid, second for frontier and goal pose plotting
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+
+    # Plot occupancy array on the first figure (ax1)
+    self.rotated_map =  np.fliplr(np.rot90(self.map_2d_occupancy_array, k=1))
+    ax1.imshow(self.rotated_map, cmap=cmap, norm=norm)
+
+    # Set the extent of ax2 to match the extent of ax1
+    ax2.set_xlim(ax1.get_xlim())
+    ax2.set_ylim(ax1.get_ylim())
+
+    # Set the aspect ratio of both axes to equal
+    ax1.set aspect('equal')
+    ax2.set aspect('equal')
+
+    # Scatter plot the unoccupied-unexplored frontiers on the second figure (ax2)
+    # Also plot the goal_pose waypoint for the densest frontier
+    if self.unoc_unex:
+        # Rotate and transform coordinates
+        unoc_unex_x, unoc_unex_y = zip(*self.unoc_unex)
+        unoc_unex_rotated= [(self.map_height - y, self.map_width - x) for x, y in zip(unoc_unex_x, unoc_unex_y)]
+        ax2.scatter(*zip(*unoc_unex_rotated), c='pink', marker='x', s=3)
+
+        # Plot goal_pose waypoint also convert to grid coordinates
+        highest_scoring frontier = max(self.dense_frontier_scores, key=self.dense_frontier_scores.get)
+        x, y = self.map_to_grid_coordinates(highest_scoring frontier)
+        rotated_highest_scoring frontier = [(self.map_height - y, self.map_width - x)]
+        ax2.scatter(*zip(*rotated_highest_scoring frontier), color='red', marker='x', linestyle='-')
+
+    # Scatter plot the unoccupied-unexplored-obstacle frontiers on the second figure (ax2)
+    if self.unoc_unex_obst:
+        # Rotate and transform coordinates
+        unoc_unex_obst_x, unoc_unex_obst_y = zip(*self.unoc_unex_obst)
+        unoc_unex_obst_rotated= [(self.map_height - y, self.map_width - x) for x, y in zip(unoc_unex_obst_x, unoc_unex_obst_y)]
+        ax2.scatter(*zip(*unoc_unex_obst_rotated), c='teal', marker='s', s=5)
+
+        # Plot wall goal pose
+        highest_scoring frontier = max(self.wall_frontier_scores, key=self.wall_frontier_scores.get)
+        x, y = self.map_to_grid_coordinates(highest_scoring frontier)
+        rotated_highest_scoring frontier = [(self.map_height - y, self.map_width - x)]
+        ax2.scatter(*zip(*rotated_highest_scoring frontier), color='yellow', marker='o', linestyle='-')
+
+    # Save file
+    save_dir = "/home/jasper/turtlebot3_ws/src/map_explorer/map_explorer"
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    save_file = os.path.join(save_dir, "map_plot.png")
+    plt.savefig(save_file, dpi=200)
+    print(f"Saved figure to {save_file}")
+    plt.close()
 
 def main(args=None):
     rclpy.init(args=args)
